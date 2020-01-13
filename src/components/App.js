46e9777a0bda4c6770f609dea5967ece633891a2
Jose <j.a.ojeda@live.com>
2020-01-13 14:21:02 -0800

    added Header to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {
   BrowserRouter as Router
 } from 'react-router-dom';
+import Header from './Header';
 
 export default function App() {
   return (
     <Router>
-      
+      <Header />
     </Router>
   );
 }
