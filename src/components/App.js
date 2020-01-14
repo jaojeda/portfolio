58bfdb282902fd7b9a9ed6a16542797f489c3009
Jose <j.a.ojeda@live.com>
2020-01-14 08:41:31 -0800

    created Header dir and moved Header.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   BrowserRouter as Router
 } from 'react-router-dom';
-import Header from './Header';
+import Header from './Header/Header';
 
 export default function App() {
   return (
