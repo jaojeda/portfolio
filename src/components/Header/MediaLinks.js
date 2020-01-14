e712a3fe11ddffdf1c12ec5f25c6f00054235163
Jose <j.a.ojeda@live.com>
2020-01-14 11:01:32 -0800

    wrote MediaLinks.js

---
+++ src/components/Header/MediaLinks.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MediaLink from './MediaLink';
+
+const MediaLinks = ({ links }) => {
+  const linkElements = links.map((link, i) => (
+    <li key={i}>
+      <MediaLink {...link} />
+    </li>
+  ));
+  return (
+    <ul>
+      {linkElements}
+    </ul>
+  );
+};
+
+MediaLinks.propTypes = {
+  links: PropTypes.arrayOf(PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired
+  })).isRequired
+};
