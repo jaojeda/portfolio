a1fb3a1c501e4bbd3ff2ce8778dca0744b12fbc8
Jose <j.a.ojeda@live.com>
2020-01-18 11:07:17 -0800

    added Route & Switch to App.js, added Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Route,
+  Switch
 } from 'react-router-dom';
 import Header from './Header/Header';
+import Home from './Home/Home';
+import About from './About/About';
 
 export default function App() {
   return (
     <Router>
       <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </Router>
   );
 }
