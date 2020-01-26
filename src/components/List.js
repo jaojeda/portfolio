a80cb181b2064a45c83a4452d02f39e17dc9cb05
Jose <j.a.ojeda@live.com>
2020-01-25 20:40:33 -0800

    wrote sanitized List.js element

---
+++ src/components/List.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const List = ({ arr }) => {
+  const listElement = arr.map((str, i) => (
+    <li key={i}>{str}</li>
+  ));
+  return (
+    <ul>
+      {listElement}
+    </ul>
+  );
+};
+
+List.propTypes = {
+  arr: PropTypes.array.isRequired
+};
