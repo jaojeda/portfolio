f8f7a91a5b0f5049597bf857323b51b3178813f9
Jose <j.a.ojeda@live.com>
2020-01-25 21:00:08 -0800

    import List to About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import List from '../List';
 
 const About = () => {
+  const techArr = ['JavaScript', 'HTML', 'CSS'];
+  const toolArr = ['Heroku', 'Netlify', 'Socket.io'];
   return (
-    <h1>About Me</h1>
+    <>
+      <h1>About Me</h1>
+      <p>Hi! My name is Jose, I'm a Full-Stack developer that enjoys creating responsive apps.</p>
+      <List arr={techArr} />
+      <List arr={toolArr} />
+    </>
   );
 };
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Projects = () => {
   return (
     <h1>Project</h1>
-  )
-  };
+  );
+};
 
 export default Projects;
