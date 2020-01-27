d9bf8ca10299849078bea674f80cafd9d0de8401
Jose <j.a.ojeda@live.com>
2020-01-26 19:58:25 -0800

    created data.js file

---
+++ data/data.js
@@ -0,0 +1,14 @@
+export const techArr = ['JavaScript', 'HTML', 'CSS'];
+
+export const toolArr = ['Heroku', 'Netlify', 'Socket.io'];
+
+export const projects = [
+  {
+    title: 'Maze-Race',
+    img: '../../../assets/maze-race.png',
+    description: 'A retro-inspired multiplayer game that allows players to invite up to eight friends to a private lobby and race against each other to see who can solve a randomly generated maze the fastest.',
+    tech: 'Node.js, Socket.io, React, P5',
+    repoLink: 'https://maze-race.netlify.com/',
+    demoLink: 'https://github.com/Team-Amazing-Race'
+  }
+];
