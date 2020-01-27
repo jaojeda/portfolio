a995525d5f90aa0f9362b98ac1b6173e120cb196
Jose <j.a.ojeda@live.com>
2020-01-26 19:19:18 -0800

    imported Project to Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Project from './Project';
 
-const Projects = () => {
+const Projects = ({ projects }) => {
+  const projectElements = projects.map((project) => (
+    <>
+      <Project {...project} />
+    </>
+  ));
+
   return (
-    <h1>Project</h1>
+    <>
+      <h1>Projects</h1>
+      <section>
+        {projectElements}
+      </section>
+    </>
   );
+};
+
+Projects.propTypes = {
+  projects: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    tech: PropTypes.array.isRequired,
+    repoLink: PropTypes.string.isRequired,
+    demoLink: PropTypes.string.isRequired
+  })).isRequired
 };
 
 export default Projects;
