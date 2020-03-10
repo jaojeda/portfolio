43dc66510d6e058888b55695f00c3d67f7eca8e1
Jose <j.a.ojeda@live.com>
2020-03-10 09:13:33 -0700

    added logo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.css';
 
 const Header = () => (
   <header className={styles.Header} >
-    <h1>Title</h1>
+    <img src='../../../assets/logo.png' />
     <Link activeClass="active" to="home" spy={true} smooth={true} offset={0} duration={500}>Home</Link>
     <Link activeClass="active" to="about" spy={true} smooth={true} offset={0} duration={500}>About</Link>
     <Link activeClass="active" to="projects" spy={true} smooth={true} offset={0} duration={500}>Projects</Link>
