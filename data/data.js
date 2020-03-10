dabea7a79115bc69b639b1f3103dff8b87959aa3
Jose <j.a.ojeda@live.com>
2020-03-09 22:53:59 -0700

    added new projects and made menu relative

--- data/data.js
+++ data/data.js
@@ -10,5 +10,29 @@ export const projects = [
     tech: 'Node.js, Socket.io, React, P5',
     repoLink: 'https://maze-race.netlify.com/',
     demoLink: 'https://github.com/Team-Amazing-Race'
-  }
+  },
+  {
+    title: 'Maze-Api',
+    img: '../../../assets/maze-api.png',
+    description: 'Maze API is a RESTful API that generates a variety of mazes from a selection of recursive algorithms. Users can sign up, receive a key, and request mazes with an extensive suite of parameters.',
+    tech: 'MongoDB, Express, Node.js',
+    repoLink: 'https://github.com/maze-api/maze-api',
+    demoLink: 'https://maze-api.herokuapp.com/'
+  },
+  {
+    title: 'Chroma',
+    img: '../../../assets/chroma.png',
+    description: 'Bored? Stimulate your curiosity with color by using Chroma. The game will give you a scrambled set of colors from a gradient, and the user will need to align them in order.',
+    tech: 'Express, Node.js, PostgreSQL',
+    repoLink: 'https://github.com/Chroma-Squad/chroma-game',
+    demoLink: 'https://chroma-game.herokuapp.com/'
+  },
+  {
+    title: 'Deja Brew',
+    img: '../../../assets/deja-brew.png',
+    description: ' Learn all about espresso-based drinks with Deja Brew We\'ll take you through the staples then move on to play mode, where you\'ll encounter customer orders, and have to beat the clock to win!',
+    tech: 'Vanilla JavaScript',
+    repoLink: 'https://github.com/team-caffeine/team-caffeine.github.io',
+    demoLink: 'https://team-caffeine.github.io'
+  },
 ];
