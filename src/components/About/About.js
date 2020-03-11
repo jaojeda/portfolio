6e780e0e016acfb5862ca98eb3ca311c2acfc098
Jose <j.a.ojeda@live.com>
2020-03-11 15:06:51 -0700

    added new lists to About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,18 +1,21 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import List from '../List';
-import { techArr, toolArr } from '../../../data/data';
+import { toolArr, backEnd, frontEnd, databases, langArr } from '../../../data/data';
 import styles from './About.css';
 
 const About = () => {
   
   return (
     <div className={styles.About} >
-      <h1 id="about">About Me</h1>
+      <h1 id="about">ABOUT ME</h1>
       <p>Hi! My name is Jose, I'm a Full-Stack developer that enjoys creating responsive apps.</p>
       <div>
-        <List arr={techArr} />
-        <List arr={toolArr} />
+        <List arr={langArr} title={'LANGUAGES'} />
+        <List arr={toolArr} title={'TOOLS'} />
+        <List arr={backEnd} title={'BACK END'} />
+        <List arr={frontEnd} title={'FRONT END'} />
+        <List arr={databases} title={'DATABASES'} />
       </div>
     </div>
   
