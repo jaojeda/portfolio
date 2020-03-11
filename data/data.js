1278761644a8536b246c20089f111d9f8e43b988
Jose <j.a.ojeda@live.com>
2020-03-11 15:06:14 -0700

    added links arr, corrected titles

--- data/data.js
+++ data/data.js
@@ -1,10 +1,16 @@
-export const techArr = ['JavaScript', 'HTML', 'CSS', 'Node.js', 'Express', 'Socket.io', 'React.js', 'Redux', 'P5', 'PostgreSQL', 'MongoDB', 'Mongoose'];
+export const langArr = ['JavaScript', 'HTML', 'CSS'];
 
-export const toolArr = ['Heroku', 'Netlify', 'Socket.io', 'Jest', 'QUnit', 'Github', 'Swagger.io'];
+export const toolArr = ['Heroku', 'Netlify', 'Jest', 'QUnit', 'Github', 'Swagger.io'];
+
+export const backEnd = ['Node.js', 'Express', 'Socket.io'];
+
+export const frontEnd = ['React.js', 'Redux', 'P5'];
+
+export const databases = ['PostgreSQL', 'MongoDB', 'Mongoose'];
 
 export const projects = [
   {
-    title: 'Maze-Race',
+    title: 'MAZE RACE',
     img: '../../../assets/maze-race.png',
     description: 'A retro-inspired multiplayer game that allows players to invite up to eight friends to a private lobby and race against each other to see who can solve a randomly generated maze the fastest.',
     tech: 'Node.js, Socket.io, React, P5',
@@ -12,7 +18,7 @@ export const projects = [
     demoLink: 'https://github.com/Team-Amazing-Race'
   },
   {
-    title: 'Maze-Api',
+    title: 'MAZE API',
     img: '../../../assets/maze-api.png',
     description: 'Maze API is a RESTful API that generates a variety of mazes from a selection of recursive algorithms. Users can sign up, receive a key, and request mazes with an extensive suite of parameters.',
     tech: 'MongoDB, Express, Node.js',
@@ -20,7 +26,7 @@ export const projects = [
     demoLink: 'https://maze-api.herokuapp.com/'
   },
   {
-    title: 'Chroma',
+    title: 'CHROMA',
     img: '../../../assets/chroma.png',
     description: 'Bored? Stimulate your curiosity with color by using Chroma. The game will give you a scrambled set of colors from a gradient, and the user will need to align them in order.',
     tech: 'Express, Node.js, PostgreSQL',
@@ -28,11 +34,26 @@ export const projects = [
     demoLink: 'https://chroma-game.herokuapp.com/'
   },
   {
-    title: 'Deja Brew',
+    title: 'DEJA BREW',
     img: '../../../assets/deja-brew.png',
     description: ' Learn all about espresso-based drinks with Deja Brew We\'ll take you through the staples then move on to play mode, where you\'ll encounter customer orders, and have to beat the clock to win!',
     tech: 'Vanilla JavaScript',
     repoLink: 'https://github.com/team-caffeine/team-caffeine.github.io',
     demoLink: 'https://team-caffeine.github.io'
   },
+];
+
+export const links = [
+  {
+    img: '../../../assets/linkedin.png',
+    link: 'https://www.linkedin.com/in/jose-a-ojeda/'
+  },
+  {
+    img: '../../../assets/twitter.png',
+    link: 'https://twitter.com/JosAOjed/'
+  },
+  {
+    img: '../../../assets/github.png',
+    link: 'https://github.com/jaojeda/'
+  }
 ];
