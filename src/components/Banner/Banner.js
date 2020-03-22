8ee8aa8328728871147602285e02f23a4bbc92cc
Jose <j.a.ojeda@live.com>
2020-03-22 15:50:10 -0700

    added chaining animations

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './Banner.css';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated, useChain } from 'react-spring';
 
 const Banner = () => {
-  const props = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 2000 } });
+  const springRef = useRef();
+  const props = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 1000 }, ref: springRef });
+
+  const transitionRef0 = useRef();
+  const transitions0 = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 1000 }, ref: transitionRef0 });
+
+  const transitionRef1 = useRef();
+  const transitions1 = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 1000 }, ref: transitionRef1 });
+  
+  
+  useChain([springRef, transitionRef0, transitionRef1], [0, 0.5, 1]);
 
   return (
     <div className={styles.Banner}>
       <animated.img style={props} className={styles.dots} src='../../../assets/geo1.png'/>
-      <animated.img style={props} className={styles.lines} src='../../../assets/geo2.png'/>
-      <animated.div style={props}></animated.div>
+      <animated.img style={transitions0} className={styles.lines} src='../../../assets/geo2.png'/>
+      <animated.div style={transitions1}></animated.div>
     </div>
   );
 
