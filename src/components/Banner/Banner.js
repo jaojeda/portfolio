025892dee4972257852b317985fffc91cac5441c
Jose <j.a.ojeda@live.com>
2020-03-23 08:54:10 -0700

    updated transition

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,25 +1,41 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styles from './Banner.css';
-import { useSpring, animated, useChain } from 'react-spring';
+import { useSpring, animated, useChain, useTransition } from 'react-spring';
 
 const Banner = () => {
+  const transitionRef = useRef();
+  const [toggle, set] = useState(false);
+  const transitions = useTransition(toggle, null, {
+    from: { position: 'absolute', opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    ref: transitionRef
+  });
+
+  useEffect(() => setTimeout(() => set(true), 3000), []);
+
   const springRef = useRef();
   const props = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 1000 }, ref: springRef });
 
-  const transitionRef0 = useRef();
-  const transitions0 = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 1000 }, ref: transitionRef0 });
+  const springRef0 = useRef();
+  const props0 = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 1000 }, ref: springRef0 });
 
-  const transitionRef1 = useRef();
-  const transitions1 = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 1000 }, ref: transitionRef1 });
+  const springRef1 = useRef();
+  const props1 = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 1000 }, ref: springRef1 });
   
   
-  useChain([springRef, transitionRef0, transitionRef1], [0, 0.5, 1]);
+  useChain([springRef, springRef0, springRef1, transitionRef], [0, 0.5, 1, 1.5]);
 
   return (
     <div className={styles.Banner}>
       <animated.img style={props} className={styles.dots} src='../../../assets/geo1.png'/>
-      <animated.img style={transitions0} className={styles.lines} src='../../../assets/geo2.png'/>
-      <animated.div style={transitions1}></animated.div>
+      <animated.img style={props0} className={styles.lines} src='../../../assets/geo2.png'/>
+      <animated.div style={props1} className={styles.para}></animated.div>
+      {transitions.map(({ item, props }) => 
+        item
+          ? <animated.div style={props}>JOSE OJEDA // FULL STACK DEVELOPER</animated.div>
+          : <animated.div style={props}>WELCOME</animated.div>
+      )}
     </div>
   );
 
