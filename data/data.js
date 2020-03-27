f97b31a143dc39616fd2fd2a2fac838a8c54506d
Jose <j.a.ojeda@live.com>
2020-03-27 09:56:41 -0700

    2nd neflify fix

--- data/data.js
+++ data/data.js
@@ -1,11 +1,3 @@
-import mazeRace from '../../../assets/maze-race.png';
-import mazeApi from '../../../assets/maze-api.png';
-import chroma from '../../../assets/chroma.png';
-import dejaBrew from '../../../assets/deja-brew.png';
-import linkedin from '../../../assets/linkedin.png';
-import twitter from '../../../assets/twitter.png';
-import github from '../../../assets/github.png';
-
 export const langArr = ['JavaScript', 'HTML', 'CSS'];
 
 export const toolArr = ['Heroku', 'Netlify', 'Jest', 'QUnit', 'Github', 'Swagger.io'];
@@ -19,7 +11,7 @@ export const databases = ['PostgreSQL', 'MongoDB', 'Mongoose'];
 export const projects = [
   {
     title: 'MAZE RACE',
-    img: mazeRace,
+    img: 'https://raw.githubusercontent.com/jaojeda/portfolio/master/assets/maze-race.png',
     description: 'A retro-inspired multiplayer game that allows players to invite up to eight friends to a private lobby and race against each other to see who can solve a randomly generated maze the fastest.',
     tech: 'Node.js, Socket.io, React, P5',
     repoLink: 'https://maze-race.netlify.com/',
@@ -27,7 +19,7 @@ export const projects = [
   },
   {
     title: 'MAZE API',
-    img: mazeApi,
+    img: 'https://raw.githubusercontent.com/jaojeda/portfolio/master/assets/maze-api.png',
     description: 'Maze API is a RESTful API that generates a variety of mazes from a selection of recursive algorithms. Users can sign up, receive a key, and request mazes with an extensive suite of parameters.',
     tech: 'MongoDB, Express, Node.js',
     repoLink: 'https://github.com/maze-api/maze-api',
@@ -35,7 +27,7 @@ export const projects = [
   },
   {
     title: 'CHROMA',
-    img: chroma,
+    img: 'https://raw.githubusercontent.com/jaojeda/portfolio/master/assets/chroma.png',
     description: 'Bored? Stimulate your curiosity with color by using Chroma. The game will give you a scrambled set of colors from a gradient, and the user will need to align them in order.',
     tech: 'Express, Node.js, PostgreSQL',
     repoLink: 'https://github.com/Chroma-Squad/chroma-game',
@@ -43,7 +35,7 @@ export const projects = [
   },
   {
     title: 'DEJA BREW',
-    img: dejaBrew,
+    img: 'https://raw.githubusercontent.com/jaojeda/portfolio/master/assets/deja-brew.png',
     description: ' Learn all about espresso-based drinks with Deja Brew We\'ll take you through the staples then move on to play mode, where you\'ll encounter customer orders, and have to beat the clock to win!',
     tech: 'Vanilla JavaScript',
     repoLink: 'https://github.com/team-caffeine/team-caffeine.github.io',
@@ -53,15 +45,15 @@ export const projects = [
 
 export const links = [
   {
-    img: linkedin,
+    img: 'https://raw.githubusercontent.com/jaojeda/portfolio/master/assets/linkedin.png',
     link: 'https://www.linkedin.com/in/jose-a-ojeda/'
   },
   {
-    img: twitter,
+    img: 'https://raw.githubusercontent.com/jaojeda/portfolio/master/assets/twitter.png',
     link: 'https://twitter.com/JosAOjed/'
   },
   {
-    img: github,
+    img: 'https://raw.githubusercontent.com/jaojeda/portfolio/master/assets/github.png',
     link: 'https://github.com/jaojeda/'
   }
 ];
